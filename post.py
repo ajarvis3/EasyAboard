9b4f655f96595b4bd981beb791b1f2a42d069fef
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2019-05-20 22:11:03 -0400

    Bots
    It doesn't seem like airlines like bots :cry:

--- post.py
+++ post.py
@@ -1,29 +1,73 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome(executable_path='C:\\Program Files\\chromedriver\\chromedriver.exe')
 
+# Finds an element by its id.
+# Sets the element to the given value. Must be a text field.
+def set_elem_to_value(id, value):
+    elem = browser.find_element_by_id(id)
+    elem.clear()
+    elem.send_keys(value)
+    elem.send_keys(Keys.ENTER)
+
 # American Airlines
-american = 'https://www.aa.com/homePage.do'
-browser.get(american)
+# american = 'https://www.aa.com/booking/find-flights'
+# browser.get(american)
 
-one_way = 'watOneWay'
-browser.find_element_by_id(one_way).click() # Selects One Way option
+# one_way = 'tabOneWay'
+# browser.find_element_by_id(one_way).click() # Selects One Way option
 
-src = 'JFK'
-src_id = 'reservationFlightSearchForm.originAirport'
-src_elem = browser.find_element_by_id(src_id)
-src_elem.clear()
-src_elem.send_keys(src)
+# src = 'JFK'
+# src_id = 'segments0.origin'
+# src_elem = browser.find_element_by_id(src_id)
+# src_elem.clear()
+# src_elem.send_keys(src)
 
 
-dest = 'LAX'
-dest_id = 'reservationFlightSearchForm.destinationAirport'
-dest_elem = browser.find_element_by_id(dest_id)
-dest_elem.clear()
-dest_elem.send_keys(dest)
+# dest = 'LAX'
+# dest_id = 'segments0.destination'
+# dest_elem = browser.find_element_by_id(dest_id)
+# dest_elem.clear()
+# dest_elem.send_keys(dest)
 
-dep = '05/15/2019'
-dep_id = 'aa-leavingOn'
-dep_elem = browser.find_element_by_id(dep_id)
-dep_elem.clear()
-dep_elem.send_keys(dep)
+# dep = '05/15/2019'
+# dep_id = 'segments0.travelDate'
+# dep_elem = browser.find_element_by_id(dep_id)
+# dep_elem.clear()
+# dep_elem.send_keys(dep)
+
+# time = '120001' # All day value
+# time_id = 'segments0.travelTime'
+# time_elem = Select(browser.find_element_by_id(time_id))
+# time_elem.select_by_value(time)
+
+# submit_id = 'flightSearchSubmitBtn'
+# submit_elem = browser.find_element_by_id(submit_id)
+# submit_elem.click()
+
+#Southwest
+southwest = 'https://www.southwest.com/air/booking/'
+browser.get(southwest)
+
+trip_type_value = 'oneway'
+trip_type_name = 'tripType'
+trip_type_elem = browser.find_element_by_name(trip_type_name)
+trip_type_elem.click()
+
+src = 'EWR'
+src_id = 'originationAirportCode'
+set_elem_to_value(src_id, src)
+
+
+dest = 'FLL'
+dest_id = 'destinationAirportCode'
+set_elem_to_value(dest_id, dest)
+
+
+dep = '06/17'
+dep_id = 'departureDate'
+set_elem_to_value(dep_id, dep)
+
+
